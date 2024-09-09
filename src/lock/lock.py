ab459e3a1c1669a22de1b9c3d36947f96e477710
morzan1001 <morzan1001@gmail.com>
2024-09-09 15:58:41 +0200

    Refactor GPIOController to use gpiod.request_lines for better line management and update gpiod version constraint in pyproject.toml

--- src/lock/lock.py
+++ src/lock/lock.py
@@ -1,24 +1,35 @@
 import gpiod
+from gpiod.line import Direction, Value
 
 class GPIOController:
     """Initialize the GPIOController with a specific chip and line number."""
     def __init__(self, chip, line_number):
-        self.chip = gpiod.chip(chip)
+        self.chip = chip
         self.line_number = line_number
-        self.line = self.chip.get_line(self.line_number)
-        config = gpiod.line_request()
-        config.consumer = 'Kiosk-Lock'
-        config.request_type = gpiod.line_request.DIRECTION_OUTPUT
-        self.line.request(config)
+        self.request = None
+        self.request_line()
+
+    def request_line(self):
+        self.request = gpiod.request_lines(
+            self.chip,
+            consumer="Kiosk-Lock",
+            config={
+                self.line_number: gpiod.LineSettings(
+                    direction=Direction.OUTPUT, 
+                    output_value=Value.INACTIVE
+                )
+            }
+        )
 
     def activate(self):
         """Set the GPIO line to HIGH (active)."""
-        self.line.set_value(1)
+        self.request.set_value(self.line_number, Value.ACTIVE)
 
     def deactivate(self):
         """Set the GPIO line to LOW (inactive)."""
-        self.line.set_value(0)  
+        self.request.set_value(self.line_number, Value.INACTIVE)
 
     def cleanup(self):
         """Release the control of the GPIO line."""
-        self.line.release() 
+        if self.request:
+            self.request.release()
